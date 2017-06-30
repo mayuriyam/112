15d62e7a3f9614df50e09e05e4c2c1428cd1e339
mayuriyam <mayuriya21@gmail.com>
2017-06-30 11:24:10 +0530

    Create reverse1.java

---
+++ reverse1.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+class reverse1
+{
+    public static void main(String ar[])
+    {
+        Scanner sc=new Scanner (System.in);
+        String a=sc.nextLine();
+        StringBuilder a1=new StringBuilder(a);
+        a1.reverse();
+     System.out.println(a1);
+        
+        
+    }
+}
